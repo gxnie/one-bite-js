6857b20fdf6b7482bd968669b27bef239e105988
genie <wlsdl7520@naver.com>
2025-04-14 22:57:59 +0900

    add: trip-wiki

---
+++ pokemon/app.js
@@ -0,0 +1,8 @@
+import Detail from "./src/components/Detail.js";
+import Header from "./src/components/Header.js";
+import Main from "./src/components/Main.js";
+import Search from "./src/components/Search.js";
+
+import { getMain, getDetail } from "./src/modules/api.js";
+
+export default function App($app) {}
---
+++ pokemon/src/modules/api.js
@@ -0,0 +1,7 @@
+export function getMain(id) {
+  // 서버에서 포켓몬 메인 리스트 정보를 받아오는 함수
+}
+
+export function getDetail(props) {
+  // 서버에서 포켓몬 상세 정보를 받아오는 함수
+}
